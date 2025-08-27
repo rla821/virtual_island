c061729c9fb7275c0311d9502d1567a46cafc84a
rla821 <43646843+rla821@users.noreply.github.com>
2025-08-27 18:05:35 +0900

    gh-pages: adapt router and public paths for GitHub Pages; update StreamerPage and MainPage to use PUBLIC_URL; improve index.html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./style.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Use HashRouter so routes work on GitHub Pages without server routing */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
