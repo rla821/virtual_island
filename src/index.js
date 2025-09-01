684fa5983b7ecfe9b0dbbf7c8e9a915a8e7af54b
rla821 <43646843+rla821@users.noreply.github.com>
2025-09-01 10:40:00 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    {/* Use HashRouter so routes work on GitHub Pages without server routing */}
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
